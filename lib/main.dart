cd1c7b7214c12db04b834c5a3e14034cc9e86d34
Everton Smith <evertonwsmith@gmail.com>
2024-02-04 15:31:40 -0800

    five

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,17 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  const firebaseConfig = {
+    'apiKey': "AIzaSyDCV-6CC4JpQd8c0dWaKRi8J8oYC0pBPb4",
+    'authDomain': "uix2024-dcdfe.firebaseapp.com",
+    'databaseURL': "https://uix2024-dcdfe-default-rtdb.firebaseio.com",
+    'projectId': "uix2024-dcdfe",
+    'storageBucket': "uix2024-dcdfe.appspot.com",
+    'messagingSenderId': "428749353952",
+    'appId': "1:428749353952:web:680e0a3e0f006731753185"
+  };
+
   runApp(const MyApp());
 }
 
@@ -28,7 +39,7 @@ class MyApp extends StatelessWidget {
         ),
         // home: login());
         home: landing(name: 'Everton'));
-        // home: game(gameModel: empt, docName: 'Everton379', user: 'Everton'));
+    // home: game(gameModel: empt, docName: 'Everton379', user: 'Everton'));
   }
 }
 
@@ -40,5 +51,6 @@ challengeModel empt = challengeModel(
   state: 'inactive%fart%dull%daft%////%////',
   challenger: 'Everton',
   challenged: ['Everton'],
-  secretWord: 'daft', id: -1,
+  secretWord: 'daft',
+  id: -1,
 );
